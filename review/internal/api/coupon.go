9ad8a5c1fb7d59501d5b36a1c571de3cb98c15b4
Marina Rukavitsyna <marina.rukavitsyna@gmail.com>
2024-05-16 22:05:13 +0200

    :recycle: introduce interface for payloads struct

--- review/internal/api/coupon.go
+++ review/internal/api/coupon.go
@@ -7,25 +7,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// RequestPayload represents the payload for applying a coupon
 type RequestPayload struct {
 	Code   string        `json:"code"`
 	Basket entity.Basket `json:"basket"`
 }
 
+// CouponCodePayload represents the payload for getting coupons by codes
 type CouponCodePayload struct {
 	Codes []string `json:"codes"`
 }
 
+// CouponPayload represents the payload for creating a coupon
 type CouponPayload struct {
 	Discount       int    `json:"discount"`
 	Code           string `json:"code"`
 	MinBasketValue int    `json:"minBasketValue"`
 }
 
+// It returns an error if the JSON parsing or binding fails
+func parsePayload(c *gin.Context, payload any) error {
+	if err := c.ShouldBindJSON(payload); err != nil {
+		return err
+	}
+	return nil
+}
+
 // ApplyCoupon handles the application of a coupon to a basket
 func (a *API) ApplyCoupon(c *gin.Context) {
-	apiReq, err := parseAppRequest(c)
-	if err != nil {
+	var apiReq RequestPayload
+	if err := parsePayload(c, &apiReq); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -41,8 +52,8 @@ func (a *API) ApplyCoupon(c *gin.Context) {
 
 // CreateCoupon handles the creation of a new coupon
 func (a *API) CreateCoupon(c *gin.Context) {
-	apiReq, err := parseCouponRequest(c)
-	if err != nil {
+	var apiReq CouponPayload
+	if err := parsePayload(c, &apiReq); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -57,8 +68,8 @@ func (a *API) CreateCoupon(c *gin.Context) {
 
 // GetCouponsByCodes handles fetching coupons by their codes
 func (a *API) GetCouponsByCodes(c *gin.Context) {
-	apiReq, err := parseCouponCodesRequest(c)
-	if err != nil {
+	var apiReq CouponCodePayload
+	if err := parsePayload(c, &apiReq); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -70,31 +81,4 @@ func (a *API) GetCouponsByCodes(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, coupons)
-}
-
-// Helper function to parse ApplicationRequest from the context
-func parseAppRequest(c *gin.Context) (RequestPayload, error) {
-	var apiReq RequestPayload
-	if err := c.ShouldBindJSON(&apiReq); err != nil {
-		return apiReq, err
-	}
-	return apiReq, nil
-}
-
-// Helper function to parse CouponRequest from the context
-func parseCouponRequest(c *gin.Context) (CouponPayload, error) {
-	var apiReq CouponPayload
-	if err := c.ShouldBindJSON(&apiReq); err != nil {
-		return apiReq, err
-	}
-	return apiReq, nil
-}
-
-// Helper function to parse CouponRequest by codes from the context
-func parseCouponCodesRequest(c *gin.Context) (CouponCodePayload, error) {
-	var apiReq CouponCodePayload
-	if err := c.ShouldBindJSON(&apiReq); err != nil {
-		return apiReq, err
-	}
-	return apiReq, nil
 }
