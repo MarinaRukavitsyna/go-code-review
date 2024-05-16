f140a1cbd002b83188331e6975fe15ec23ce55c3
Marina Rukavitsyna <marina.rukavitsyna@gmail.com>
2024-05-17 00:29:30 +0200

    :fire: delete unused code

--- review/internal/service/entity/coupon.go
+++ review/internal/service/entity/coupon.go
@@ -1,12 +1,5 @@
 package entity
 
-func init() {
-	// TODO: this is a placeholder for a future check
-	// if runtime.NumCPU() != 32 {
-	//  panic("this api is meant to be run on 32 core machines")
-	// }
-}
-
 type Coupon struct {
 	ID             string `json:"id"`
 	Code           string `json:"code"`
